f7aeadfcee7ff1e21f005d5cf2292d766bc80f1e
HajarBenbounjima04 <hajarbenbonjima@gmail.com>
2026-02-21 15:27:29 +0000

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
-dotenv.config();
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.use(cors());
+connectDB();
+
+const allowedOrigins = ["http://localhost:5173"];
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-app.get("/", (req, res) => {
-  res.send("API running...");
-});
+app.get("/", (req, res) => res.send("API Working"));
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(port, () => console.log("Server Started"));
