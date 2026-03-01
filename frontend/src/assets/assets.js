7f88fd6c03d0194b2f1bd18fb01604b2246fde18
HajarBenbounjima04 <hajarbenbonjima@gmail.com>
2026-03-01 13:43:25 +0000

    Add static assets (images, JSON files)

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,26 @@
+import arrow_icon from "./arrow_icon.svg";
+import lock_icon from "./lock_icon.svg";
+import viva from "./VivaCosmetics.png";
+import mail_icon from "./mail_icon.svg";
+import person_icon from "./person_icon.svg";
+import hand_wave from "./hand_wave.png";
+import header_img from "./header_img.png";
+import Hydrolate from "./HYDROLAT-2.jpg";
+import Argan_Products from "./Argan_Products.png";
+import oil from "./oil.png";
+import Parapharma from "./Parapharma.png";
+import Skin_care from "./Skin_care.png";
+export const assets = {
+  arrow_icon,
+  lock_icon,
+  viva,
+  mail_icon,
+  person_icon,
+  hand_wave,
+  header_img,
+  Hydrolate,
+  Argan_Products,
+  oil,
+  Parapharma,
+  Skin_care,
+};
