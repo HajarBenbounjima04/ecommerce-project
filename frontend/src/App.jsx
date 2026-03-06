bf5a2ff42bdfaba4f691416b73e501e4cf57ef00
HajarBenbounjima04 <hajarbenbonjima@gmail.com>
2026-03-06 10:58:21 +0000

    update App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-
+import AboutUs from "./pages/aboutUs";
+import ContactUs from "./pages/ContactUs";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
@@ -10,6 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+
       </Routes>
     </div>
   );
