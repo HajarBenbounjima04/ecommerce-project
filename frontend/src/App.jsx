0d9aa9f7aa41cd91a4173156e988b7298de25373
HajarBenbounjima04 <hajarbenbonjima@gmail.com>
2026-03-09 18:36:52 +0000

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import VerifyEmail from "./pages/VerifyEmail";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer, toast } from "react-toastify";
+import AddProduct from "./AddProduct";
+import Wishlist from "./pages/Wishlist";
+import Cart from "./pages/Cart";
+import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
 import AboutUs from "./pages/aboutUs";
 import ContactUs from "./pages/ContactUs";
-import ResetPassword from "./pages/ResetPassword";
-import Shop from "./pages/Shop";
-import AddProduct from "./AddProduct";
-import { ToastContainer } from "react-toastify";
+import Checkout from "./pages/Checkout";
 
 const App = () => {
   return (
@@ -15,11 +21,16 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/verify-email" element={<VerifyEmail />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+        <Route path="/addProduct" element={<AddProduct />}></Route>
+        <Route path="/wishlist" element={<Wishlist />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/product/:identifier" element={<ProductDetails />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="/reset-password" element={<ResetPassword />}></Route>
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/add-product" element={<AddProduct />} /> 
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
   );
